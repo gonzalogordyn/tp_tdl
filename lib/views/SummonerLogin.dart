54f6f3a1d6ff326be33dfc7844e23ee243a41683
gonzalogordyn <ggordyn@fi.uba.ar>
2022-06-06 00:50:04 -0300

    Retoques a SummonerLogin

--- lib/views/SummonerLogin.dart
+++ lib/views/SummonerLogin.dart
@@ -14,32 +14,24 @@ class _SummonerInputScreenState extends State<SummonerInputScreen> {
 
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('GG.OP',
-          style: TextStyle(
-            fontSize: 30.0,
-            letterSpacing: 2.0,
-            fontFamily: 'Roboto-Black',
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.grey[900],
-      ),
-      body: Container(
+      backgroundColor: const Color(0xff003d73),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            const SizedBox(
+              height: 100.0,
+            ),
             SizedBox(
               width: 200.0,
-              height: 300.0,
+              height: 200.0,
               child: Image.asset('assets/pengu.png'),
             ),
             Row(
                 children: <Widget>[
                   Container(
-                    padding: const EdgeInsets.all(30.0),
+                    padding: const EdgeInsets.fromLTRB(30.0, 30.0, 20.0, 30.0),
                     child: SizedBox(
                       width: 250.0,
                       child: TextField(
@@ -47,33 +39,52 @@ class _SummonerInputScreenState extends State<SummonerInputScreen> {
                           summonerName = text;
                         },
                         decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(100.0)),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          focusColor: Colors.white,
                           labelText: 'Enter summoner name',
                         ),
                       ),
                     ),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      height: 60.0,
-                      width: 60.0,
-                      child: ServerDropdownMenu(),
-
-                    ),
+                  Container(
+                      width: 80.0,
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                          color: Colors.white,
+                      ),
+                      child: const Center(child: ServerDropdownMenu()),
                   ),
 
                 ]
             ),
+            const SizedBox(
+                height: 30.0,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                  primary: Colors.blue[700],
+                  elevation: 0,
+              ),
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(5, 15, 5, 15),
+                child: Text(
+                    'SEARCH',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Roboto-Black.ttf',
+                        fontWeight: FontWeight.bold,
+                    ),
+                ),
+              ),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          print(summonerName);
-        },
-        child: Text('GO'),
-        backgroundColor: Colors.blue[500],
       ),
     );
   }
@@ -91,31 +102,32 @@ class _ServerDropdownMenu extends State<ServerDropdownMenu> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      focusColor: Colors.amber,
-      icon: const Icon(Icons.expand_more_rounded),
-      elevation: 4,
-      alignment: AlignmentDirectional.topStart,
-      menuMaxHeight: 200.0,
-      style: const TextStyle(
-          color: Colors.black54,
-          fontWeight: FontWeight.bold,
-      ),
-      borderRadius: BorderRadius.circular(10.0),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
+    return DropdownButtonHideUnderline(
+      child: DropdownButton<String>(
+        value: dropdownValue,
+        icon: const Icon(Icons.expand_more_rounded),
+        elevation: 4,
+        alignment: AlignmentDirectional.topStart,
+        menuMaxHeight: 200.0,
+        style: const TextStyle(
+            color: Colors.black54,
+            fontWeight: FontWeight.bold,
+        ),
+        borderRadius: BorderRadius.circular(10.0),
+        onChanged: (String? newValue) {
+          setState(() {
+            dropdownValue = newValue!;
+          });
 
-      },
-      items: <String>['LAS', 'NA', 'EUW', 'EUNE', 'LAN', 'BR', 'JP', 'TR']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
+        },
+        items: <String>['LAS', 'NA', 'EUW', 'EUNE', 'LAN', 'BR', 'JP', 'TR']
+            .map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+      ),
     );
   }
 }
