804c2c8dac142930da7c99a25e701101d9d6fc84
manudelca <manueldelcarril@gmail.com>
2022-06-17 15:20:12 -0300

    Match details has state

--- lib/views/match_details.dart
+++ lib/views/match_details.dart
@@ -6,31 +6,31 @@ import 'navigation_drawer.dart';
 import '../components/user_header.dart';
 import '../components/match_details/match_summary.dart';
 
-class MatchDetails extends StatelessWidget {
+class MatchDetails extends StatefulWidget {
+  final Match match;
+  final MatchParticipant matchParticipant;
+  final Summoner summoner;
+
   const MatchDetails({Key? key,
     required this.match,
     required this.matchParticipant,
     required this.summoner}) : super(key: key);
 
+  @override
+  _MatchDetails createState() => _MatchDetails();
+}
 
-  final Match match;
-  final MatchParticipant matchParticipant;
-  final Summoner summoner;
-  static const Color winnerColor = Color(0xff92DEF6);
-  static const Color looserColor = Color(0xffFB9191);
-  static const Color playerWinnerColor = Color(0xff456bf8);
-  static const Color playerLooserColor = Color(0xffff4040);
-
+class _MatchDetails extends State<MatchDetails> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color(0xff263F65),
         drawer: NavigationDrawer(),
-        appBar: userHeader(summoner.summonerIconId!, summoner.summonerLevel!),
-        body: MatchSummary(matchParticipant: matchParticipant,
-            match: match,
-            summoner: summoner),
+        appBar: userHeader(widget.summoner.summonerIconId!, widget.summoner.summonerLevel!),
+        body: MatchSummary(matchParticipant: widget.matchParticipant,
+            match: widget.match,
+            summoner: widget.summoner),
     );
   }
 }
