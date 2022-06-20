80e8b5045855b433965c42ebc65d3a6056edb7b8
manudelca <manueldelcarril@gmail.com>
2022-06-20 00:14:22 -0300

    Bar colors

--- lib/components/match_details/match_analysis.dart
+++ lib/components/match_details/match_analysis.dart
@@ -6,6 +6,8 @@ import '../../model/match/match.dart';
 class MatchAnalysis extends StatelessWidget {
   final Match match;
   final MatchParticipant matchParticipant;
+  static const Color winColor = Color(0xff456bf8);
+  static const Color looseColor = Color(0xffff4040);
 
   const MatchAnalysis({Key? key,
     required this.match,
@@ -14,37 +16,44 @@ class MatchAnalysis extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final List<ChartData> data = _buildTotalDamageDealtToChampions();
-
-    return SfCartesianChart(
-              title: ChartTitle(text: "Damage done"),
-              primaryXAxis: CategoryAxis(majorGridLines: MajorGridLines(width: 0),
-                                         axisLine: AxisLine(width: 0), ),
-              primaryYAxis: NumericAxis(majorGridLines: MajorGridLines(width: 0),
-                                        axisLine: AxisLine(width: 0)),
-        series: <ChartSeries>[
-                BarSeries<ChartData, String>(
-                    dataSource: data,
-                    xValueMapper: (ChartData data, _) => data.x,
-                    yValueMapper: (ChartData data, _) => data.y,
-                    borderRadius: BorderRadius.all(Radius.circular(15))
-                )
-              ]
-            );
+    final List<ChartData> damageDealtData = buildTotalDamageDealtToChampions();
+    return Column(
+              children: [
+                buildDataBar(damageDealtData),
+              ],
+          );
   }
 
-  List<ChartData> _buildTotalDamageDealtToChampions() {
-    List<MatchParticipant> participants = List.from(match.participants);
-    participants.sort((a, b) => a.totalDamageDealtToChampions.compareTo(b.totalDamageDealtToChampions));
-    return participants.map((e) => ChartData(x: e.championName, y: e.totalDamageDealtToChampions)).toList();
+  List<ChartData> buildTotalDamageDealtToChampions() {
+    List<ChartData> participants = match.participants
+        .map((e) => ChartData(x: e.championName, y: e.totalDamageDealtToChampions, color: (e.win ? winColor : looseColor)))
+        .toList();
+    participants.sort((a, b) => a.y.compareTo(b.y));
+    return participants;
+  }
+
+  SfCartesianChart buildDataBar(List<ChartData> data) {
+    return SfCartesianChart(
+          plotAreaBorderWidth: 0,
+          primaryXAxis: CategoryAxis(isVisible: true),
+          primaryYAxis: NumericAxis(isVisible: false),
+          series: <ChartSeries>[
+            BarSeries<ChartData, String>(
+              dataSource: data,
+              xValueMapper: (ChartData data, _) => data.x,
+              yValueMapper: (ChartData data, _) => data.y,
+              pointColorMapper: (ChartData data, _) => data.color,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            )
+          ]
+      );
   }
 }
-
-
 
 class ChartData {
   final String x;
   final int y;
+  final Color color;
 
-  const ChartData({required this.x, required this.y});
+  const ChartData({required this.x, required this.y, required this.color});
 }
